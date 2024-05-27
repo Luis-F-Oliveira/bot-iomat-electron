1093f2e25989273ce4bf839a4427e1f4d078cdaf
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2024-05-27 17:10:27 -0300

    configurando layout do admin

---
+++ src/app/admin/navbar.tsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+  return (
+    <nav className="flex justify-around items-center border-b-2 py-3">
+      <h1 className="text-xl">
+        Facilita Diário
+      </h1>
+      <ul>
+        <li>
+          aqui vai ficar as páginas
+        </li>
+      </ul>
+    </nav>
+  )
+}
---
+++ src/app/admin/upbutton.tsx
@@ -0,0 +1,10 @@
+import { ChevronUp } from "lucide-react"
+
+export const UpButton = () => {
+  return (
+    <ChevronUp 
+      className="border-2 rounded-full py-1 fixed bottom-5 right-5 cursor-pointer" 
+      size={30} 
+    />
+  )
+}
