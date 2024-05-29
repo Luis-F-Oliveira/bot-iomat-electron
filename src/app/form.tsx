fd9c11692f58016f23102f272ba40149f4b5aa29
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2024-05-29 17:44:44 -0300

    finalizando sistema de login

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -18,28 +18,45 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+import { api } from "@/lib/axios"
+import { toast } from "@/components/ui/use-toast"
 
 const FormSchema = z.object({
-  pin: z.string().min(6, {
+  entry_code: z.string().min(6, {
     message: "Código de acesso deve conter 6 caracteres."
   })
 })
 
 export function LoginForm() {
+  const router = useRouter()
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      pin: ""
+      entry_code: ""
     }
   })
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    console.log(JSON.stringify(data, null, 2))
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    const pin = JSON.stringify(data, null, 2)
+
+    const response = await api.post('auth', pin)
+    const { auth } = response.data
+
+    if (auth.length !== 0) {
+      localStorage.setItem('user', JSON.stringify(auth[0]))
+      router.push('/dashboard')
+    } else {
+      toast({
+        title: "Erro ao cadastrar",
+        description: "Usuário não encontrado!"
+      })
+    }
   }
 
   useEffect(() => {
     const subscription = form.watch((value) => {
-      if (value.pin && value.pin.length === 6) {
+      if (value.entry_code && value.entry_code.length === 6) {
         form.handleSubmit(onSubmit)()
       }
     })
@@ -51,7 +68,7 @@ export function LoginForm() {
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-3">
         <FormField
           control={form.control}
-          name="pin"
+          name="entry_code"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Código de entrada</FormLabel>
@@ -73,7 +90,7 @@ export function LoginForm() {
                         field.onChange(e)
                         const value = target.value
                         if (value.length === 1) {
-                          const pinValue = form.getValues("pin")
+                          const pinValue = form.getValues("entry_code")
                           if (pinValue.length === 6) {
                             form.handleSubmit(onSubmit)()
                           }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
+import { Toaster } from "@/components/ui/toaster"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -21,11 +22,12 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased dark",
+          "min-h-screen bg-background font-sans antialiased dark overflow-x-hidden",
           fontSans.variable
         )}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   )
